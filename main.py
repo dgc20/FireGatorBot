f3cf4262bb6ab7ce46178f02c67bd8bf49a2e9d5
DGC20 <dgc20@pitt.edu>
2024-07-27 19:54:16 -0400

    d

---
+++ main.py
@@ -0,0 +1,21 @@
+import tweepy
+import os
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def twitterConnection(creds):
+    bearer_token = os.environ.get()
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    print(os.environ['TWITTER_AT'])
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
